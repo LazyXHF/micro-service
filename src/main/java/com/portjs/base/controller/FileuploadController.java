4ee8893abe4cd67d09aeb9f6bf733f82c247cdde
gumingyang <550284612@qq.com>
2019-03-14 08:56:25 +0800

    上传跨域注解

--- src/main/java/com/portjs/base/controller/FileuploadController.java
+++ src/main/java/com/portjs/base/controller/FileuploadController.java
@@ -7,6 +7,7 @@ import com.portjs.base.util.StringUtils.StringUtils;
 import com.portjs.base.util.Upload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @author gumingyang
  **/
 @Controller
+@CrossOrigin
 @RequestMapping("/uploadFile")
 public class FileuploadController extends BaseController {
     static final  String TAG = "FileuploadController================>";
