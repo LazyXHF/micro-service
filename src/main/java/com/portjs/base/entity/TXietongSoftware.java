915a1df5f195033f8e112423cacfe93e17603961
qiuxin <1692377169@qq.com>
2019-03-25 13:40:18 +0800

    qx- 项目概览，合同，报表首页，动态跟踪首页

---
+++ src/main/java/com/portjs/base/entity/TXietongSoftware.java
@@ -0,0 +1,89 @@
+package com.portjs.base.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.ibatis.type.Alias;
+@Entity
+@Table(name = "t_xietong_software")
+@Alias(value = "TXietongSoftware")
+public class TXietongSoftware {
+	private static final long serialVersionUID = -8304467058421560175L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String softwareName;
+
+    private String softwareVersion;
+
+    private String downloadPath;
+
+    private Integer downloadTimes;
+
+    private Integer isdelete;
+
+    private Date createtime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getSoftwareName() {
+        return softwareName;
+    }
+
+    public void setSoftwareName(String softwareName) {
+        this.softwareName = softwareName == null ? null : softwareName.trim();
+    }
+
+    public String getSoftwareVersion() {
+        return softwareVersion;
+    }
+
+    public void setSoftwareVersion(String softwareVersion) {
+        this.softwareVersion = softwareVersion == null ? null : softwareVersion.trim();
+    }
+
+    public String getDownloadPath() {
+        return downloadPath;
+    }
+
+    public void setDownloadPath(String downloadPath) {
+        this.downloadPath = downloadPath == null ? null : downloadPath.trim();
+    }
+
+    public Integer getDownloadTimes() {
+        return downloadTimes;
+    }
+
+    public void setDownloadTimes(Integer downloadTimes) {
+        this.downloadTimes = downloadTimes;
+    }
+
+    public Integer getIsdelete() {
+        return isdelete;
+    }
+
+    public void setIsdelete(Integer isdelete) {
+        this.isdelete = isdelete;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+}
---
+++ src/main/java/com/portjs/base/service/DownloadSoftwareService.java
@@ -0,0 +1,26 @@
+package com.portjs.base.service;
+
+import java.util.List;
+
+
+import com.portjs.base.entity.TXietongSoftware;
+import com.portjs.base.util.ResponseMessage;
+
+
+public interface DownloadSoftwareService {
+
+	//页面展示数据
+	List<TXietongSoftware> selectAllSoftware();
+
+	//新增软件
+	ResponseMessage insertSoftware(String filePath, String softwareName, String softwareVersion);
+	//删除
+	int deleteSoftware(String id);
+	//模糊查询
+	List<TXietongSoftware> selectBySoftwareName(String softwareName);
+	//增加下载次数
+	int updateByDownload(String id);
+	//查重
+	List<TXietongSoftware> selectDuplicate(String softwareName, String softwareVersion);
+
+}
