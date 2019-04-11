dff0e40db6fc55fff523fa8502e246ee65215d86
qiuxin <1692377169@qq.com>
2019-04-11 10:47:52 +0800

    QX------

--- src/main/java/com/portjs/base/dao/PurchaseRequestMapper.java
+++ src/main/java/com/portjs/base/dao/PurchaseRequestMapper.java
@@ -1,35 +1,61 @@
 package com.portjs.base.dao;
 
 import com.portjs.base.entity.PurchaseRequest;
-import com.portjs.base.entity.PurchaseRequestExample;
-import com.portjs.base.util.ResponseMessage;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
 public interface PurchaseRequestMapper {
-    int countByExample(PurchaseRequestExample example);
-
-    int deleteByExample(PurchaseRequestExample example);
-
     int deleteByPrimaryKey(String id);
 
     int insert(PurchaseRequest record);
 
     int insertSelective(PurchaseRequest record);
 
-    List<PurchaseRequest> selectByExample(PurchaseRequestExample example);
-
     PurchaseRequest selectByPrimaryKey(String id);
-
-    int updateByExampleSelective(@Param("record") PurchaseRequest record, @Param("example") PurchaseRequestExample example);
-
-    int updateByExample(@Param("record") PurchaseRequest record, @Param("example") PurchaseRequestExample example);
-
-    int updateByPrimaryKeySelective(PurchaseRequest record);
 
     int updateByPrimaryKey(PurchaseRequest record);
 
-    List queryPucharseList(@Param("method") String method, @Param("projectCode") String projectCode,@Param("projectName") String projectName);
+    int updateByPrimaryKeySelective(PurchaseRequest record);
+
+    /**
+     * 采购清单列表统计
+     * @param projectId
+     * @param requestId
+     * @return
+     */
+    int purchaseRequestCounts(@Param("projectId") String projectId, @Param("requestId") String requestId);
+
+    /**
+     * 采购清单列表的分页
+     * @param projectId
+     * @param requestId
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    List<PurchaseRequest> queryPurchaseRequestInfo(@Param("projectId") String projectId,@Param("requestId") String requestId,
+                                             @Param("pageNo") Integer pageNo,@Param("pageSize") Integer pageSize);
+    /**
+     * 批量软删除
+     * @param ids
+     * @return
+     */
+    int updateDeleteTimeByIds(@Param("id") List<String> ids);
+
+    PurchaseRequest queryPurchaseRequestById(@Param("id") String ids);
+
+    int insertPurchaseRequestSelective(PurchaseRequest record);
+
+    /**
+     * 查询最大流水单号
+     * @return
+     */
+    @Select("select max(request_num) from purchase_request")
+    String findMaxOdd();
+
+    List queryPucharseList(String method, String projectCode, String projectName);
 }
