b704ca2db0994e3f5a254115bb4cf4dc36decc81
ruanshuhao <981208382@qq.com>
2019-04-16 13:49:53 +0800

    user信息下拉

--- src/main/java/com/portjs/base/dao/InternalAttachmentMapper.java
+++ src/main/java/com/portjs/base/dao/InternalAttachmentMapper.java
@@ -35,4 +35,6 @@ public interface InternalAttachmentMapper {
     List<InternalAttachment> queryProjectRecords(String id);
 
     int updateByrelateddomainId(InternalAttachment internalAttachment);
+
+    List<InternalAttachment> queryPucharseReviewFiles(@Param("id") String id);
 }
