39a2370407f8cda51b009525af73af0463d9cbf8
gumingyang <550284612@qq.com>
2019-04-18 19:05:45 +0800

    message

---
+++ src/main/java/com/portjs/base/util/Message/MessageUtils.java
@@ -0,0 +1,18 @@
+package com.portjs.base.util.Message;
+
+/**
+ * @author gumingyang
+ * @description 返回信息
+ **/
+public  class MessageUtils {
+    public final static String NOT_PASSED = "未传";
+
+    public final static String NOT_EXIST = "不存在";
+
+    public final static String UNKNOWN_EXCEPITON = "未知异常";
+
+    public final static String RETURNED_SUCCESSFULLY = "退回成功";
+
+    public final static String SUCCESS = "操作成功";
+
+}
