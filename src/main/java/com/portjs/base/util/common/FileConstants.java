e6d17bc67b965cbf09f9c68bef45445f5af6e998
gumingyang <550284612@qq.com>
2019-04-19 13:33:33 +0800

    上传文件加入文件大小和文件姓名长度以及合法性校验

---
+++ src/main/java/com/portjs/base/util/common/FileConstants.java
@@ -0,0 +1,19 @@
+package com.portjs.base.util.common;
+
+/**
+ * @author gumingyang
+ * @description 上传文件通用数据
+ **/
+public class FileConstants {
+    /**
+     * 默认的文件名最大长度
+     */
+    public static final int DEFAULT_FILE_NAME_LENGTH = 200;
+
+    /**
+     * 默认大小 50M
+     */
+    public static final long DEFAULT_MAX_SIZE = 52428800;
+
+
+}
