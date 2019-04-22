42b13b4a7156e9a023b877ee84d1bc5855a57ab0
gumingyang <550284612@qq.com>
2019-04-22 10:46:32 +0800

    待办跳转bug

--- src/main/java/com/portjs/base/controller/TenderController.java
+++ src/main/java/com/portjs/base/controller/TenderController.java
@@ -69,6 +69,8 @@ public class TenderController extends BaseController {
     @ResponseBody
     public ResponseMessage queryTender(@RequestBody String requestBody){
         try {
+
+
             logger.error(TAG+"query-tender()begin....."+requestBody);
             return tenderService.queryTender(requestBody);
         } catch (Exception e) {
