e1806be643d9109e1d0c737a4cd32e918d5b6e6b
gumingyang <550284612@qq.com>
2019-04-25 10:08:28 +0800

    生成uuid(去除横岗)

---
+++ src/main/java/com/portjs/base/util/UUID/UuidPlus.java
@@ -0,0 +1,20 @@
+package com.portjs.base.util.UUID;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * @author gumingyang
+ * @description 去除UUID中的横岗
+ **/
+public class UuidPlus {
+    private static String getUUIDPlus() {
+        String src = UUID.randomUUID().toString();
+        return src.substring(0, 8) + src.substring(9, 13) + src.substring(14, 18) + src.substring(19, 23) + src.substring(24, 36);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(UuidPlus.getUUIDPlus());
+    }
+
+}
