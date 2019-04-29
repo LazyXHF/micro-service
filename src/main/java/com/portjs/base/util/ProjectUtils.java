13821434bfb4af05c5c7b357a7be168577fe3712
daiyueyuan <1242882585@qq.com>
2019-04-29 19:51:29 +0800

    项目概览

--- src/main/java/com/portjs/base/util/ProjectUtils.java
+++ src/main/java/com/portjs/base/util/ProjectUtils.java
@@ -56,79 +56,85 @@ public class ProjectUtils {
         if (projectApprovalType != null) {
             project.setProjectApprovalType(projectApprovalType);
         }
-        String status = project.getProjectStatus();
-        //这个存放第一种情况
-        StringBuffer buffer = new StringBuffer();
-        //这个存放第二种情况
-        StringBuffer buffer2 = new StringBuffer();
-        String[] statusArray = status.split(",");
-        for (int i = 0; i < statusArray.length; i++) {
-            //如果有就替换，没有就在尾部添加
-            String astatus = statusArray[i].substring(0, 2);
-            if (projectStatus.substring(0, 2).equals(astatus)) {
-                String replaceStatus = statusArray[i].substring(0);
-                String status2 = status.replace(replaceStatus, projectStatus);
-                buffer.append(status2);
-                break;
+        if (project.getProjectApprovalType() != 2) {
+            String status = project.getProjectStatus();
+            //这个存放第一种情况
+            StringBuffer buffer = new StringBuffer();
+            //这个存放第二种情况
+            StringBuffer buffer2 = new StringBuffer();
+            String[] statusArray = status.split(",");
+            for (int i = 0; i < statusArray.length; i++) {
+                //如果有就替换，没有就在尾部添加
+                String astatus = statusArray[i].substring(0, 2);
+                if (projectStatus.substring(0, 2).equals(astatus)) {
+                    String replaceStatus = statusArray[i].substring(0);
+                    String status2 = status.replace(replaceStatus, projectStatus);
+                    buffer.append(status2);
+                    break;
+                }
+                //如果没有就一只添加，直到最后一个也没有就追加
+                else {
+                    buffer2.append(statusArray[i]).append(",");
+                    if (i == (statusArray.length - 1)) {
+                        buffer2.append(projectStatus);
+                    }
+                }
             }
-            //如果没有就一只添加，直到最后一个也没有就追加
+
+
+            //如果状态的长度没有发生变化了   说明替换了  用buffer
+            if (status.length() == buffer.toString().length()) {
+                project.setProjectCode(projectCode);
+                project.setSchedule(schedule);
+                project.setProjectStatus(buffer.toString());
+                String[] statuss = project.getProjectStatus().split(",");
+                for (String prostatus : statuss) {
+                    if ("3".equals(prostatus.substring(2, 3))) {
+                        project.setStatus("3");
+                        break;
+                    } else if ("0".equals(prostatus.substring(2, 3))) {
+                        project.setStatus("0");
+                        break;
+                    } else if ("2".equals(prostatus.substring(2, 3))) {
+                        project.setStatus("2");
+                    } else {
+                        project.setStatus("1");
+                    }
+                }
+                projectMapper.updateByPrimaryKeySelective(project);
+            }
+            //如果变了，说明追加了用新的
             else {
-                buffer2.append(statusArray[i]).append(",");
-                if (i == (statusArray.length - 1)) {
-                    buffer2.append(projectStatus);
+                project.setProjectCode(projectCode);
+                project.setSchedule(schedule);
+                project.setProjectStatus(buffer2.toString());
+                String[] statuss = project.getProjectStatus().split(",");
+                for (String prostatus : statuss) {
+                    if ("3".equals(prostatus.substring(2, 3))) {
+                        project.setStatus("3");
+                        break;
+                    } else if ("0".equals(prostatus.substring(2, 3))) {
+                        project.setStatus("0");
+                        break;
+                    } else if ("2".equals(prostatus.substring(2, 3))) {
+                        project.setStatus("2");
+                    } else {
+                        project.setStatus("1");
+                    }
                 }
+                projectMapper.updateByPrimaryKeySelective(project);
             }
-        }
 
-
-        //如果状态的长度没有发生变化了   说明替换了  用buffer
-        if (status.length() == buffer.toString().length()) {
+            BusinessConfiguration businessConfiguration = businessConfigurationMapper.selectByprostatus(projectStatus.substring(0, 1),
+                    projectId, projectStatus.substring(0, 2));
+            if (businessConfiguration != null) {
+                businessConfiguration.setNode(projectStatus);
+                businessConfigurationMapper.updateByPrimaryKeySelective(businessConfiguration);
+            }
+        } else {
             project.setProjectCode(projectCode);
             project.setSchedule(schedule);
-            project.setProjectStatus(buffer.toString());
-            String[] statuss = project.getProjectStatus().split(",");
-            for (String prostatus : statuss) {
-                if ("3".equals(prostatus.substring(2, 3))) {
-                    project.setStatus("3");
-                    break;
-                } else if ("0".equals(prostatus.substring(2, 3))) {
-                    project.setStatus("0");
-                    break;
-                } else if ("2".equals(prostatus.substring(2, 3))) {
-                    project.setStatus("2");
-                } else {
-                    project.setStatus("1");
-                }
-            }
             projectMapper.updateByPrimaryKeySelective(project);
-        }
-        //如果变了，说明追加了用新的
-        else {
-            project.setProjectCode(projectCode);
-            project.setSchedule(schedule);
-            project.setProjectStatus(buffer2.toString());
-            String[] statuss = project.getProjectStatus().split(",");
-            for (String prostatus : statuss) {
-                if ("3".equals(prostatus.substring(2, 3))) {
-                    project.setStatus("3");
-                    break;
-                } else if ("0".equals(prostatus.substring(2, 3))) {
-                    project.setStatus("0");
-                    break;
-                } else if ("2".equals(prostatus.substring(2, 3))) {
-                    project.setStatus("2");
-                } else {
-                    project.setStatus("1");
-                }
-            }
-            projectMapper.updateByPrimaryKeySelective(project);
-        }
-
-        BusinessConfiguration businessConfiguration = businessConfigurationMapper.selectByprostatus(projectStatus.substring(0, 1),
-                projectId, projectStatus.substring(0, 2));
-        if (businessConfiguration != null) {
-            businessConfiguration.setNode(projectStatus);
-            businessConfigurationMapper.updateByPrimaryKeySelective(businessConfiguration);
         }
     }
 }
